894a4e8e30e49bd7c60c5541c18db6fcc4ba7094
kimjaemin <jaeminkim@hansol.com>
2024-12-11 16:08:12 +0900

    initialize 디렉토리 추가

--- src/service/fs/fs-global.ts
+++ src/service/fs/fs-global.ts
@@ -7,6 +7,16 @@ export const RepositoryDirectory = process.env.WD_REPOSITORY_DIR;
 export const DefaultUserName = process.env.WD_DEFAULT_USERNAME;
 export const OutputDirectory = process.env.WD_OUTPUT_DIR;
 
+// 초기화
+const initializeDirectories = () => {
+    const baseDir = path.join('/tmp', 'repo');
+    if (!fs.existsSync(baseDir)) {
+        fs.mkdirSync(baseDir, { recursive: true });
+        logger.debug(`Base directory created: ${baseDir}`);
+    }
+}
+
+initializeDirectories();
 export const listdir = (sourcePath: string) => {
     const prefix = "listWorkingDirectory";
     let dirs: string[] = [];
@@ -14,6 +24,12 @@ export const listdir = (sourcePath: string) => {
     logger.debug(`path: ${sourcePath}`, { prefix: prefix });
 
     try {
+        // 디렉토리 확인 및 생성
+        if (!fs.existsSync(sourcePath)) {
+            fs.mkdirSync(sourcePath, { recursive: true });
+            logger.debug(`Directory created: ${sourcePath}`, { prefix : prefix });
+        }
+
         let files = fs.readdirSync(sourcePath);
         dirs.push(...files.filter((fileName) => fs.lstatSync(path.join(sourcePath, fileName)).isDirectory()));
     } catch (error: any) {
